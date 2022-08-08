e392550006f3bc9170ba1a4808886fcde86ab4fb
ronin0322 <772875964@qq.com>
2022-08-08 09:04:15 +0800

    copylistwithrandompointer138

--- copy_list_with_random_pointer_138/solution.go
+++ copy_list_with_random_pointer_138/solution.go
@@ -2,7 +2,7 @@
  * @Author: ronin0322
  * @Date: 2022-08-08 00:30:52
  * @LastEditors: ronin0322
- * @LastEditTime: 2022-08-08 00:30:52
+ * @LastEditTime: 2022-08-08 09:04:01
  * @FilePath: /leetcode/copy_list_with_random_pointer_138/solution.go
  * @Description:
  *
@@ -37,4 +37,48 @@ func copyRandomList(head *Node) *Node {
 	}
 	return root.Next
 
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList2(head *Node) *Node {
+	root := head
+	if head == nil {
+		return nil
+	}
+	for root != nil {
+		newNode := &Node{root.Val, root.Next, nil}
+		root.Next = newNode
+		root = root.Next.Next
+	}
+	root = head
+	for root != nil {
+		if root.Random != nil {
+			root.Next.Random = root.Random.Next
+		} else {
+			root.Next.Random = nil
+		}
+
+		root = root.Next.Next
+	}
+	root = head
+	res := root.Next
+	for root != nil {
+		nxt := root.Next.Next
+		if root.Next.Next == nil {
+			root.Next.Next = nil
+		} else {
+			root.Next.Next = root.Next.Next.Next
+		}
+		root.Next = nxt
+		root = root.Next
+	}
+	return res
 }
