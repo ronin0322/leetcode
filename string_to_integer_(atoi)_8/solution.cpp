9f8a20a84a92e0397c62040431f25261ec555cc3
ronin0322 <772875964@qq.com>
2022-08-16 10:22:39 +0800

    8

--- string_to_integer_(atoi)_8/solution.cpp
+++ string_to_integer_(atoi)_8/solution.cpp
@@ -1,16 +1,15 @@
 /*
  * @Author: ronin0322
- * @Date: 2022-08-16 09:43:29
+ * @Date: 2022-08-16 10:19:48
  * @LastEditors: ronin0322
- * @LastEditTime: 2022-08-16 10:15:29
- * @FilePath: /leetcode/main.cpp
+ * @LastEditTime: 2022-08-16 10:22:05
+ * @FilePath: /leetcode/string_to_integer_(atoi)_8/solution.cpp
  * @Description:
  *
  * Copyright (c) 2022 by ronin0322, All Rights Reserved.
  */
-#include <cstdio>
-#include <cstring>
-#include <iostream>
+
+#include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
@@ -20,7 +19,7 @@ public:
         int n = s.size();
         int index = 0;
         int sign = 1;
-        long long num = 0;
+        int num = 0;
         for (; index < n && s[index] == ' ';)
             index++;
         if (index < n && s[index] == '+')
@@ -33,8 +32,13 @@ public:
             sign = -1;
             index++;
         }
+        cout << sign << index;
         for (; index < n && s[index] >= '0' && s[index] <= '9';)
         {
+            if (num * sign > INT_MAX / 10 || (num * sign == INT_MAX / 10 && (s[index] - '0') > INT_MAX % 10))
+                return INT_MAX;
+            if (num * sign < INT_MIN / 10 || (num * sign == INT_MIN / 10 && (s[index] - '0') > 7))
+                return INT_MIN;
             num = num * 10 + (s[index] - '0');
             index++;
         }
