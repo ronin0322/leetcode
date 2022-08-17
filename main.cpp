70e05e1bfa53d505f43f2b3e93bb3f65c3eaa66d
ronin0322 <772875964@qq.com>
2022-08-17 09:53:06 +0800

    8

--- main.cpp
+++ main.cpp
@@ -1,43 +1,65 @@
 /*
  * @Author: ronin0322
- * @Date: 2022-08-16 09:43:29
+ * @Date: 2022-08-17 09:22:27
  * @LastEditors: ronin0322
- * @LastEditTime: 2022-08-16 10:15:29
+ * @LastEditTime: 2022-08-17 09:49:00
  * @FilePath: /leetcode/main.cpp
  * @Description:
  *
  * Copyright (c) 2022 by ronin0322, All Rights Reserved.
  */
-#include <cstdio>
-#include <cstring>
-#include <iostream>
+
+#include <bits/stdc++.h>
+
 using namespace std;
-class Solution
+
+void pri(vector<int> &queue)
 {
-public:
-    int myAtoi(string s)
-    {
-        int n = s.size();
-        int index = 0;
-        int sign = 1;
-        long long num = 0;
-        for (; index < n && s[index] == ' ';)
-            index++;
-        if (index < n && s[index] == '+')
-        {
-            sign = 1;
-            index++;
-        }
-        else if (index < n && s[index] == '-')
-        {
-            sign = -1;
-            index++;
-        }
-        for (; index < n && s[index] >= '0' && s[index] <= '9';)
-        {
-            num = num * 10 + (s[index] - '0');
-            index++;
-        }
-        return num * sign;
-    }
-};
+    for (auto i = queue.begin(); i != queue.end(); i++)
+        cout << *i << " " << endl;
+}
+
+int main()
+{
+    cout << "vector" << endl;
+    vector<int> queue;
+    queue.push_back(0);
+    queue.push_back(0);
+    queue.push_back(0);
+    queue[0] = 1;
+    queue[1] = 2;
+    queue[2] = 3;
+    // queue.push_back(1);
+    // queue.push_back(2);
+    // queue.push_back(3);
+    pri(queue);
+    cout << queue.back() << endl;
+    queue.pop_back();
+    pri(queue);
+    cout << "map" << endl;
+    unordered_map<int, int> ma;
+    ma[1] = 1;
+    ma[2] = 2;
+    cout << ma[1] << endl;
+    cout << ma[2] << endl;
+    cout << ma[3] << endl;
+    cout << ma[4] << endl;
+    for (auto x = ma.begin(); x != ma.end(); x++)
+        cout << x->first << " " << x->second << endl;
+    cout << ma.size() << endl;
+
+    cout << "stack" << endl;
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    // cout << s.pop() << endl;
+    cout << "stack" << endl;
+    
+    return 0;
+}
